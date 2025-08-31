038486a4cce74388c8449704696811adf14f8a3e
stan <stan@stanbox.space>
2025-08-31 04:10:08 +0300

    init

---
+++ hw01-gradle/src/main/groovy/com/example/HelloOtus.groovy
@@ -0,0 +1,7 @@
+import com.google.common.base.CaseFormat
+
+static void main(String[] args) {
+    println "Working with guava collection"
+    println(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "test_data"))
+
+}
